d8ed4d90e41de291deafaf0d969a79451e3284a7
JH Ahn <0625dkswjdgn@g.skku.edu>
2023-12-18 05:47:00 +0900

    image처리

--- src/Chatbot.js
+++ src/Chatbot.js
@@ -5,31 +5,51 @@ import { Link } from 'react-router-dom';
 
 const api_key = process.env.REACT_APP_OPENAI_API_KEY; // API 키 환경 변수에서 로드
 
-const Message = ({ isUser, text }) => {
+const Message = ({ isUser, text, type }) => {
+  // URL을 찾아 하이퍼링크로 변환
+  const formatLink = (text) => {
+    const urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#/%=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])|(\bwww\.[a-z0-9-]+\.[a-z0-9\/\-]+)/ig;
+    return text.split(urlRegex).map((part, index) => {
+      if (part && part.match(urlRegex)) {
+        // URL이 'http' 또는 'https'로 시작하지 않는 경우에만 'http://' 추가
+        const url = part.startsWith('http') ? part : `http://${part}`;
+        return <a key={index} href={url} target="_blank" rel="noopener noreferrer">{part}</a>;
+        
+      } else {
+        // URL이 아니면 일반 텍스트
+        return part;
+      }
+    });
+  };
+
+  // 코드 블록, 문단 분리, URL 링크 처리 및 번호 목록 굵게 표시
   const formatText = (text) => {
-    // 코드 블록과 문단을 분리
-    const parts = text.split(/(```.*?```)/gs).map((part, index) => {
+    return text.split(/(```.*?```)/gs).map((part, index) => {
       if (part.startsWith("```") && part.endsWith("```")) {
-        // 코드 블록 처리
         return <pre key={index}><code>{part.slice(3, -3)}</code></pre>;
       } else {
-        // 일반 텍스트와 번호 매기기
         return part.split(/\n/).map((line, lineIndex) => {
+          // 번호 목록 감지 및 굵게 표시
           if (line.match(/^\d+\./)) {
-            // 번호 매기기 리스트 아이템 처리
-            return <p key={`${index}-${lineIndex}`}><strong>{line}</strong></p>;
+            return <p key={`${index}-${lineIndex}`}><strong>{formatLink(line)}</strong></p>;
           }
-          return <p key={`${index}-${lineIndex}`}>{line}</p>;
+          return <p key={`${index}-${lineIndex}`}>{formatLink(line)}</p>;
         });
       }
     });
-    
-    return parts;
+  };
+
+  const renderContent = () => {
+    if (type === 'image') {
+      return <img src={text} alt="Uploaded" style={{ maxWidth: '100%' }} />;
+    } else {
+      return formatText(text);
+    }
   };
 
   return (
     <div className={`message ${isUser ? 'user' : 'gpt'}`}>
-      {formatText(text)}
+      {renderContent()}
     </div>
   );
 };
@@ -46,19 +66,22 @@ const MessageList = ({ messages }) => {
     return (
       <div className="message-list">
         {messages.map((msg, index) => (
-          <Message key={index} isUser={msg.isUser} text={msg.text} />
+          <Message key={index} isUser={msg.isUser} text={msg.text} type={msg.type} />
         ))}
         <div ref={messagesEndRef} /> {/* 스크롤 이동을 위한 빈 div */}
       </div>
     );
   };
 
-const InputBox = ({ onSendMessage }) => {
+const TextInputBox = ({ onSendMessage, setInputType }) => {
   const [message, setMessage] = useState('');
+  //const [inputType, setInputType] = useState('text'); // 입력 타입
 
   const handleSend = () => {
-    onSendMessage(message);
-    setMessage('');
+    if (message.trim()) {
+      onSendMessage(message);
+      setMessage('');
+    }
   };
 
   return (
@@ -69,10 +92,47 @@ const InputBox = ({ onSendMessage }) => {
         onChange={(e) => setMessage(e.target.value)}
         onKeyPress={(e) => e.key === 'Enter' && handleSend()}
       />
+      <button onClick={() => setInputType('image')} className="button">Switch to Image Upload</button>
       <SendButton onSend={handleSend} />
     </div>
   );
 };
+
+const ImageInputBox = ({ onSendImage, setInputType }) => {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const fileInputRef = useRef(null); // 파일 입력 필드 참조 생성
+
+  const handleFileChange = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      setSelectedFile(event.target.files[0]);
+    }
+  };
+
+  const handleSendImage = () => {
+    if (selectedFile) {
+      onSendImage(selectedFile);
+      setSelectedFile(null); // 선택된 파일 초기화
+      if (fileInputRef.current) {
+        fileInputRef.current.value = ""; // 파일 입력 필드 초기화
+      }
+    }
+  };
+
+  return (
+    <div className="input-box">
+      <input
+        ref={fileInputRef}
+        type="file"
+        accept="image/*"
+        onChange={handleFileChange}
+      />    
+      <button onClick={() => setInputType('text')} className="button">Switch to Text Input</button>
+      <button onClick={handleSendImage} className="button">Send</button>
+    </div>
+  );
+};
+
+
 
 const SendButton = ({ onSend }) => (
   <button onClick={onSend} className="send-button">
@@ -85,6 +145,7 @@ const Chatbot = () => {
   const [isLoading, setIsLoading] = useState(false); // 로딩 상태
   const [summaries, setSummaries] = useState([]);
   const [shouldFetchSummary, setShouldFetchSummary] = useState(false);
+  const [inputType, setInputType] = useState('text');
 
   const handleSendMessage = async (text) => {
     if (text.trim()) {
@@ -92,18 +153,36 @@ const Chatbot = () => {
       setMessages(newMessages);
       setIsLoading(true);
 
-      const chatMessages = newMessages.map((m) => ({
-        role: m.isUser ? 'user' : 'assistant',
-        content: `${m.text}에 대한 답변을 생성해줘. 가독성을 위해 답변을 1.~, 2.~ 형태로 번호를 달아서 생성해줘. 그리고 문단을 띄워줘.`
-      }));
+      const chatMessages = newMessages.slice(-2).map((m) => {
+        if (m.isUser) {
+          let content;
+          if (m.text.length <= 10) {
+            content = `${m.text}에 대해 짧은 답변을 생성해줘.`
+          } else {
+            content = `${m.text}에 대해 대답해줘. 답변이 길다면 1.~ 2.~ 이런 식으로 번호를 매겨줘. 가독성을 위해 문단을 구분해줘. 추천할 웹사이트가 있다면 링크를 추천해줘. 이 메시지에 대한 내용은 답변에 중복 생성하지 마.`;
+          }
+          return {
+            role: 'user',
+            content: content
+          };
+        } else {
+          // GPT의 응답 메시지 (이 부분은 일반적으로 포함되지 않음)
+          return {
+            role: 'assistant',
+            content: `${m.text}에 대해 친절하게 설명드리겠습니다.`
+          };
+        }
+      });
+
+      console.log({chatMessages});
 
       const requestBody = {
-        model: "gpt-3.5-turbo",
+        model: "ft:gpt-3.5-turbo-0613:shinkisa::8Wn3OFbA",
         //messages: newMessages.map(m => ({role: m.isUser ? 'user' : 'assistant', content: chatMessages})),
         messages: chatMessages,
         //max_tokens: 300,
       };
-  
+      
       try {
         const response = await fetch('https://api.openai.com/v1/chat/completions', {
           method: 'POST',
@@ -153,7 +232,7 @@ const Chatbot = () => {
     }];
   
     const summaryRequestBody = {
-      model: "gpt-3.5-turbo",
+      model: "ft:gpt-3.5-turbo-0613:shinkisa::8Wn3OFbA",
       messages: summaryMessages,
       //max_tokens: 150,
     };
@@ -190,6 +269,63 @@ const Chatbot = () => {
     }
   };
 
+  const handleImageUpload = async (file) => {
+    const reader = new FileReader();
+    reader.onloadend = async () => {
+      const imageURL = reader.result;
+      const newMessage = {
+        text: imageURL, // 이미지의 Data URL
+        isUser: true,
+        type: 'image'
+      };
+      const updatedMessages = [...messages, newMessage];
+      setMessages(updatedMessages);
+      localStorage.setItem('chatMessages', JSON.stringify(updatedMessages));
+      setIsLoading(true);
+
+      const requestBody = {
+        model: "gpt-4-vision-preview",
+        messages: [
+          {
+            "role": "user",
+            "content": [
+              {"type": "text", "text": "이 이미지에 대해서 300자 이내로 간단히 설명해줘. 만약 code error에 관한 것이라면 해결법을 알려줘. 1.~, 2.~ 이렇게 번호를 매겨서 설명해줘. 가독성을 위해 문단을 구분해줘. 이 메시지에 대한 내용은 답변에 중복 생성하지 마."},
+              {"type": "image_url", "image_url":{"url": imageURL}}
+            ]
+          }
+        ], max_tokens: 500,
+      };
+
+      try {
+        const response = await fetch('https://api.openai.com/v1/chat/completions', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${api_key}`
+          },
+          body: JSON.stringify(requestBody)
+        });
+  
+        const data = await response.json();
+        if (data.choices && data.choices.length > 0 && data.choices[0].message) {
+          setShouldFetchSummary(true);
+          const descriptionMessage = {
+            text: data.choices[0].message.content, 
+            isUser: false,
+            type: 'text' // 이 부분은 텍스트 메시지로 처리
+          };
+          setMessages(prevMessages => [...prevMessages, descriptionMessage]);
+          localStorage.setItem('chatMessages', JSON.stringify([...updatedMessages, descriptionMessage]));     
+        } else {
+          console.error('Invalid response from the API:', data);
+        }
+      } catch (error) {
+        console.error('Error sending image:', error);
+      }
+      setIsLoading(false);
+    };
+    reader.readAsDataURL(file);
+  };
 
   const clearAll = () => {
     setMessages([]);
@@ -224,6 +360,8 @@ const Chatbot = () => {
     }
   }, [messages, shouldFetchSummary]);
 
+  
+
 
   return (
     <div className="chatbot">
@@ -234,7 +372,10 @@ const Chatbot = () => {
       </div>
     <div className="chat-container">
       <MessageList messages={messages} />
-      <InputBox onSendMessage={handleSendMessage} />
+      {inputType === 'text' ?
+      <TextInputBox onSendMessage={handleSendMessage} setInputType={setInputType}/> :
+      <ImageInputBox onSendImage={handleImageUpload} setInputType={setInputType}/>
+    }
       {isLoading && <div className="loading">답변을 생성중입니다...</div>}
     </div>
       <div className="summary-container">
